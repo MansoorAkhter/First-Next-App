9e390c4ceb26d48a8fd07bb208e9de1e5406ad33
Mansoor Akhter <mansoor21st@Gmail.com>
2023-02-17 14:45:53 +0500

    feb 17

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Mansoor Akhter';
+const name = 'MansoorAkhter';
 export const siteTitle = 'Mansoor';
 
 export default function Layout({ children, home }) {
